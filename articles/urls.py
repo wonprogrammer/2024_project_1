4b1b03d2f812e27db92490a3ee84bde704a8e871
leehyewon <tnddl2345@gmail.com>
2024-03-25 18:31:49 +0900

    Modify url namespace html 수정

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('index/', views.index),
     path('dinner/<str:name>/', views.dinner),
     path('dinner_price/<str:name>/', views.dinner_price),
-    path('review/', views.review),
-    path('create_review/', views.create_review),
+    path('review/', views.review, name='review'),
+    path('create_review/', views.create_review, name='create_review'),
 ]
