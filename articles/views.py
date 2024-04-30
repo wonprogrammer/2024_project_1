24a5e0b7eb94d4604c70eca2c14ed8e8fccadd97
leehyewon <tnddl2345@gmail.com>
2024-04-30 14:13:01 +0900

    Modify Article DB Read

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import random
+from .models import Article
 
 # Create your views here.
 def index(request):
@@ -20,10 +21,12 @@ def dinner(request, name):
 def dinner_price(request, name):
     menus = [{"name":'한식 정식',"price":30000}, {"name":'중식 정식',"price":20000}, {"name":'일식 정식',"price":70000}, {"name":'분식 정식',"price":8000}]
     pick = random.choice(menus)
+    articles = Article.objects.all()
     context = {
         'pick' : pick,
         'name' : name,
         'menus' : menus,
+        'articles'  :articles,
     }
     return render(request, 'dinner_price.html', context)
 
